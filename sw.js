8220b5004e37407b5070aa86d8ae07bb2bcd0368
BuddyOfficial <imad1992le@gmail.com>
2025-08-17 12:13:42 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ const CACHE_NAME = 'dogstube-cache-v1';
 const urlsToCache = [
   '/',
   '/?utm_source=homescreen',
-  'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEinWQx-YTgrISjMnxHBD2AB5BVgQK2I2r-HyShxdoA1Uq8TddQwcHX73oDOlOd8xiDzaez1xlDMhCF7MDRdsUx1pDzTEl-dQMJy0HjH-mfmEtqNywMPhAEkTBm_qB-_tCow7Ka5kv88lDvMb2i1dqPs71Phjagv4FpWFpftjXu3Lgmnk9Zogn89Mdrh/w192-h192-p-k-no-nu/ICON.png',
-  'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEinWQx-YTgrISjMnxHBD2AB5BVgQK2I2r-HyShxdoA1Uq8TddQwcHX73oDOlOd8xiDzaez1xlDMhCF7MDRdsUx1pDzTEl-dQMJy0HjH-mfmEtqNywMPhAEkTBm_qB-_tCow7Ka5kv88lDvMb2i1dqPs71Phjagv4FpWFpftjXu3Lgmnk9Zogn89Mdrh/w512-h512-p-k-no-nu/ICON.png'
+  'https://cdn.dogz.cc/ICON92.png',
+  'https://cdn.dogz.cc/ICON12.png'
 ];
 
 self.addEventListener('install', event => {
@@ -19,3 +19,4 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
